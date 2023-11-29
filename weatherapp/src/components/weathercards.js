ad62e0460203509a6d4cc0a7480eb76e9fe14cf7
rd1073 <rd1073@srmist.edu.in>
2023-11-30 01:23:49 +0530

    commits

--- weatherapp/src/components/weathercards.js
+++ weatherapp/src/components/weathercards.js
@@ -5,19 +5,18 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-
+ import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
  
 
 const Weathercards = () => {
   return (
-    <div style={{ display: 'grid', marginTop:23 ,gridTemplateColumns: 'repeat(6, 1fr)', gap: '16px' }}>
+    <div style={{ margin: '0 20px', display: 'grid' ,gridTemplateColumns: 'repeat(6, 1fr)', gap: '22px', marginTop: 70, marginX: 'auto' }}>
 
       <Card sx={{ minWidth: 275, backgroundColor: '#e0e0e0'}}>
         
         <CardContent>
         
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign={'center'} gutterBottom>
             Word of the Day
           </Typography>
         
@@ -37,13 +36,13 @@ const Weathercards = () => {
         
         <CardContent>
         
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign={'center'} gutterBottom>
+            Date
           </Typography>
         
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:2}}>
+      <WbSunnyOutlinedIcon sx={{ fontSize: 56 /* Adjust the size as needed */ }} />
+    </Box>
         
           <Typography variant="body2">
             well meaning and kindly.
@@ -57,9 +56,13 @@ const Weathercards = () => {
         
         <CardContent>
         
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign={'center'} gutterBottom>
+          Date
+
           </Typography>
+          <Box sx={{ marginTop:2,display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <WbSunnyOutlinedIcon sx={{ fontSize: 56 /* Adjust the size as needed */ }} />
+    </Box>
         
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
             adjective
@@ -75,9 +78,13 @@ const Weathercards = () => {
         
         <CardContent>
         
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign={'center'} gutterBottom>
+          Date
+
           </Typography>
+          <Box sx={{ marginTop:2,display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <WbSunnyOutlinedIcon sx={{ fontSize: 56 /* Adjust the size as needed */ }} />
+    </Box>
         
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
             adjective
@@ -93,9 +100,13 @@ const Weathercards = () => {
         
         <CardContent>
         
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign={'center'} gutterBottom>
+          Date
+
           </Typography>
+          <Box sx={{ marginTop:2,display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <WbSunnyOutlinedIcon sx={{ fontSize: 56 /* Adjust the size as needed */ }} />
+    </Box>
         
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
             adjective
@@ -111,9 +122,13 @@ const Weathercards = () => {
         
         <CardContent>
         
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" textAlign={'center'} gutterBottom>
+          Date
+
           </Typography>
+          <Box sx={{ marginTop:2,display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <WbSunnyOutlinedIcon sx={{ fontSize: 56 /* Adjust the size as needed */ }} />
+    </Box>
         
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
             adjective
